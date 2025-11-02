72a227bf41805266c762bede06082ecd1a1311a9
Shivaraj Hiremath <shivaraj94826@gmail.com>
2025-11-02 19:52:29 +0530

    window tracker project

---
+++ window-tracker/src/WindowTracker.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function WindowTracker() {
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+    
+    React.useEffect(() => {
+        function watchWindowWidth () {
+            console.log("Resized")
+            setWindowWidth(window.innerWidth)
+        }
+        window.addEventListener("resize", watchWindowWidth)
+        return function() {
+            console.log("Cleaning up...")
+            window.removeEventListener("resize", watchWindowWidth)
+        }
+    }, [])
+    
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
+
+
+
+// export default function WindowTracker() {
+//     const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+
+//     React.useEffect(() => {
+//         window.addEventListener("resize", function() {
+//             setWindowWidth(window.innerWidth)
+//         })
+//     }, [])
